aa89fbde11d46e60215f25cbd3c28295dffa8475
Dalton Woodside-Moffatt <dwoodsidemoffatt@gmail.com>
2021-05-18 20:42:14 -0700

    Started install.sh

---
+++ scripts/install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+function InstallDeps()
+{
+  echo "--- Installing Dependencies ---"
+
+  sudo apt update
+  sudo apt -y upgrade
+
+  sudo apt install -y python3-pip
+  sudo apt install -y libopenjp2-7-dev
+
+  pip3 install --upgrade pip
+  pip3 install -r ../requirements.txt
+}
+
+function InstallMain()
+{
+  echo "--- Installing Macro-Zero ---"
+}
+
+function InstallServices()
+{
+  echo "--- Installing Macro-Zero Services ---"
+}
+
+if [ $# -ne 1 ]; then
+    echo "1 argument required of either [all|depsonly|main|services]"
+    exit 1
+fi
+
+
+if [[ ("$1" != "all" && "$1" != "depsonly" && "$1" != "main" && "$1" != "services") ]]; then
+  echo "1 argument required of either [all|depsonly|main|services]"
+  exit 1
+fi
+
+case $1 in
+"all")
+InstallDeps
+InstallMain
+InstallServices
+;;
+"depsonly")
+InstallDeps ;;
+"main")
+InstallMain ;;
+"services")
+InstallServices ;;
+esac
