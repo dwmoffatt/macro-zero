598d15d6c1247a8084ec50ef8b6c7e669ea1ff2f
Dalton Woodside-Moffatt <dwoodsidemoffatt@gmail.com>
2021-06-12 22:56:51 -0700

    Added more tests

--- tests/unit/RGBDriver/test_build_output_bank.py
+++ tests/unit/RGBDriver/test_build_output_bank.py
@@ -1,7 +1,6 @@
 import pytest
 from src.macrozero import MacroZero
-
-# from src.modules.RGBDriver import BANK_A, BANK_B
+from src.modules.RGBDriver import BANK_A, BANK_B
 
 
 class TestBuildOutputBank:
@@ -11,32 +10,32 @@ class TestBuildOutputBank:
     def setup_method(self, method):
         self.app = MacroZero(test_env=True, run_webserver=False)
 
-    # @pytest.mark.parametrize(
-    #     "test_input,expected",
-    #     [
-    #         ((bin(0b00000000), bin(0b00000000)), (0b00000000, 0b00000000)),
-    #        ((bin(0b11111111), bin(0b00000000)), (0b11111111, 0b00000000)),
-    #         ((bin(0b11111111), bin(0b11111111)), (0b11111111, 0b11111111)),
-    #        ((bin(0b00000000), bin(0b11111111)), (0b00000000, 0b11111111)),
-    #     ],
-    # )
-    # def test_build_output_bank_valid_bank(self, test_input, expected):
-    #     """
-    #     Tests that what is present in output_mappings is return
-    #     :return:
-    #     """
-    #     print(enumerate(test_input[0]))
-    #     for i in enumerate(test_input[0]):
-    #         self.app.rgb_driver.output_mappings[f"Bank {BANK_A}"][f"GP{i[0]}"]["Value"] = i[1]
-    #
-    #     for i in enumerate(test_input[1]):
-    #         self.app.rgb_driver.output_mappings[f"Bank {BANK_B}"][f"GP{i[0]}"]["Value"] = i[1]
-    #
-    #     result = self.app.rgb_driver.build_output_bank(BANK_A)
-    #     assert result == expected[0]
-    #
-    #     result = self.app.rgb_driver.build_output_bank(BANK_B)
-    #     assert result == expected[1]
+    @pytest.mark.parametrize(
+        "test_input,expected",
+        [
+            (("00000000", "00000000"), (0b00000000, 0b00000000)),
+            (("11111111", "00000000"), (0b11111111, 0b00000000)),
+            (("11111111", "11111111"), (0b11111111, 0b11111111)),
+            (("00000000", "11111111"), (0b00000000, 0b11111111)),
+            (("11010110", "01010001"), (0b11010110, 0b01010001)),
+        ],
+    )
+    def test_build_output_bank_valid_bank(self, test_input, expected):
+        """
+        Tests that what is present in output_mappings is return
+        :return:
+        """
+        for i in enumerate(test_input[0]):
+            self.app.rgb_driver.output_mappings[f"Bank {BANK_A}"][f"GP{7 - i[0]}"]["Value"] = int(i[1])
+
+        for i in enumerate(test_input[1]):
+            self.app.rgb_driver.output_mappings[f"Bank {BANK_B}"][f"GP{7 - i[0]}"]["Value"] = int(i[1])
+
+        result = self.app.rgb_driver.build_output_bank(BANK_A)
+        assert result == expected[0]
+
+        result = self.app.rgb_driver.build_output_bank(BANK_B)
+        assert result == expected[1]
 
     def test_build_output_bank_raise_value_error_invalid_bank(self):
         """
--- tests/unit/mkeyboard/test_build_report.py
+++ tests/unit/mkeyboard/test_build_report.py
@@ -1,3 +1,4 @@
+import pytest
 from src.macrozero import MacroZero
 from src.modules.mkeyboard import KEY_NONE, KEY_ENTER, NONE_REPORT, KEY_H, KEY_E, KEY_1, KEY_MOD_LSHIFT, KEY_BACKSLASH
 
@@ -9,45 +10,25 @@ class TestVerifyReport:
     def setup_method(self, method):
         self.app = MacroZero(test_env=True, run_webserver=False)
 
-    def test_build_report_correct_report_generated(self):
+    @pytest.mark.parametrize(
+        "test_input,expected",
+        [
+            ("Enter", KEY_NONE + KEY_NONE + KEY_ENTER + (KEY_NONE * 5)),
+            (None, NONE_REPORT),
+            ("H", KEY_MOD_LSHIFT + KEY_NONE + KEY_H + (KEY_NONE * 5)),
+            ("e", KEY_NONE + KEY_NONE + KEY_E + (KEY_NONE * 5)),
+            ("!", KEY_MOD_LSHIFT + KEY_NONE + KEY_1 + (KEY_NONE * 5)),
+            ("\\", KEY_NONE + KEY_NONE + KEY_BACKSLASH + (KEY_NONE * 5)),
+            ("|", KEY_MOD_LSHIFT + KEY_NONE + KEY_BACKSLASH + (KEY_NONE * 5)),
+        ],
+    )
+    def test_build_report_correct_report_generated(self, test_input, expected):
         """
         Tests that build_report returns correct report for given value
         :return:
         """
-        test_value = "Enter"
-        test_report = KEY_NONE + KEY_NONE + KEY_ENTER + (KEY_NONE * 5)
-        result = self.app.mkeyboard.build_report(test_value)
-        assert result == test_report
-
-        test_value = None
-        test_report = NONE_REPORT
-        result = self.app.mkeyboard.build_report(test_value)
-        assert result == test_report
-
-        test_value = "H"
-        test_report = KEY_MOD_LSHIFT + KEY_NONE + KEY_H + (KEY_NONE * 5)
-        result = self.app.mkeyboard.build_report(test_value)
-        assert result == test_report
-
-        test_value = "e"
-        test_report = KEY_NONE + KEY_NONE + KEY_E + (KEY_NONE * 5)
-        result = self.app.mkeyboard.build_report(test_value)
-        assert result == test_report
-
-        test_value = "!"
-        test_report = KEY_MOD_LSHIFT + KEY_NONE + KEY_1 + (KEY_NONE * 5)
-        result = self.app.mkeyboard.build_report(test_value)
-        assert result == test_report
-
-        test_value = "\\"
-        test_report = KEY_NONE + KEY_NONE + KEY_BACKSLASH + (KEY_NONE * 5)
-        result = self.app.mkeyboard.build_report(test_value)
-        assert result == test_report
-
-        test_value = "|"
-        test_report = KEY_MOD_LSHIFT + KEY_NONE + KEY_BACKSLASH + (KEY_NONE * 5)
-        result = self.app.mkeyboard.build_report(test_value)
-        assert result == test_report
+        result = self.app.mkeyboard.build_report(test_input)
+        assert result == expected
 
     def teardown_method(self, method):
         del self.app
