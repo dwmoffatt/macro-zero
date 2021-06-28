975416ca375735a965bddbc29ace37e50304498e
Dalton Woodside-Moffatt <dwoodsidemoffatt@gmail.com>
2021-06-27 17:25:40 -0700

    Added React frontend

--- src/backend/api.py
+++ src/backend/api.py
@@ -1,13 +1,27 @@
-from flask import Flask
+import sqlite3 as sql
+from flask import Flask, jsonify
 
 
 class API:
-    def __init__(self):
+    def __init__(self, thread_lock=None, que=None):
+        self._thread_lock = thread_lock
+        self._que = que
+
         self.server = Flask(__name__)
-        self.server.add_url_rule("/", "hello", self.hello_world)
+        self.server.add_url_rule("/api/v1/hello", "hello", self.hello_world, methods=["GET"])
+
+        self.con = sql.connect("macro-zero.db")
+
+    def init_db(self):
+        """
+        Create tables
+
+        :return:
+        """
 
     def run_api(self):
         self.server.run(host="0.0.0.0")
 
-    def hello_world(self):
-        return "Hello World!!"
+    @staticmethod
+    def hello_world():
+        return jsonify({"message": "Hello World!!"})
